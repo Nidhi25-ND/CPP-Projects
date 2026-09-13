1eefdbb978d5e43530fb0ce06e9aed9651da3e81
Nidhi Dudhankar <nidhi.rjnd@gmail.com>
2026-09-13 18:49:11 +0530

    Add Gallons to Litres conversion program

---
+++ GallonsToLitres.c
@@ -0,0 +1,11 @@
+#include<iostream>
+
+int main(){
+using namespace std;
+float gallons, litres;
+cout<<"enter gallons: ";
+cin>>gallons;
+litres = gallons * 3.7854;
+cout<<"litres: "<<litres;
+return 0;
+}
